7ac5724a62bff341d3be5f234f89f802ebbf2acc
AntP0 <frosia-ksiysha@mail.ru>
2022-04-09 17:06:12 +0300

    Добавьте файлы проекта.

---
+++ Danil(NPK)WebApplication/Pages/DTP.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Danil_NPK_WebApplication.Pages
+{
+    public class DTPModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
